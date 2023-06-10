29dd361a12db602a2072a93cf85ef966abccaaf8
Michael Oduah <oduahmichaelebuka@gmail.com>
2023-06-10 10:52:41 +0100

    Added layouts

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,15 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname');
+            $table->string('phone')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('account_type');
+            $table->string('address')->nullable();
+            $table->string('saved_items')->nullable();;
+            $table->string('cart')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
