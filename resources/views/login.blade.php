23bf1cc88db038ac950131ed5dcd57904231cfb1
Michael Oduah <oduahmichaelebuka@gmail.com>
2023-06-11 17:52:33 +0100

    Completed the register form

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,10 +4,10 @@
     <form class="login-form" method="POST" action="" enctype="multipart/form-data">
         @csrf
         <label class="login-label" for="email">Email:</label><br>
-        <input class="login-input" id="email" type="email" required><br>
+        <input name="email" class="login-input" id="email" type="email" required><br>
 
         <label class="login-label" for="password">Password:</label><br>
-        <input class="login-input" id="password" type="password" required><br>
+        <input name="password" class="login-input" id="password" type="password" required><br>
 
         <button class="btn" type="submit">Login</button>
     </form>
